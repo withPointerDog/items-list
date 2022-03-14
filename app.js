f0b262ca0a8894891dd6c1cd4825d6e42ab6c642
withPointerDog <shchedrovnewbox@gmail.com>
2022-03-14 18:05:35 +0300

    edit item

--- app.js
+++ app.js
@@ -57,6 +57,18 @@ function dsplTxtMssg(text, clss = "success") {
   setBackToDefault();
   localStorage.removeItem("list");
 }
+//edit item
+function editItem(event) {
+  const element = event.currentTarget.parentElement.parentElement;
+  // set edit item
+  editElement = event.currentTarget.parentElement.previousElementSibling;
+  // set form value
+  grocery.value = editElement.innerHTML;
+  editFlag = true;
+  editID = element.dataset.id;
+  //
+  sbmtBtn.textContent = "edit";
+}
 
 function setBackToDefault() {
   input.value = "";
@@ -102,3 +114,41 @@ function editLocalStorage(id, value) {
   localStorage.setItem("list", JSON.stringify(items));
 }
 //setup items
+function setupItems() {
+  let items = getLocalStorage();
+
+  if (items.length > 0) {
+    items.forEach((item) => {
+      createListItem(item.id, item.value);
+    });
+    container.classList.add("show-container");
+  }
+}
+
+function createListItem(id, value) {
+  const element = document.createElement("article");
+  let attr = document.createAttribute("data-id");
+  attr.value = id;
+  element.setAttributeNode(attr);
+  element.classList.add("grocery-item");
+  element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <!-- edit btn -->
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <!-- delete btn -->
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>
+          `;
+  // add event listeners to both buttons;
+  const deleteBtn = element.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", deleteItem);
+  const editBtn = element.querySelector(".edit-btn");
+  editBtn.addEventListener("click", editItem);
+
+  // append child
+  list.appendChild(element);
+}
